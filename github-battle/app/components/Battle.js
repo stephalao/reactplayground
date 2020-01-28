18d606df27cb0a3d48e9f9fae2ff3f132a977c2f
stephenson <salao@debt.com>
2020-01-28 00:25:30 -0500

    results skeleton

--- github-battle/app/components/Battle.js
+++ github-battle/app/components/Battle.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa'
 import PropTypes from 'prop-types'
+import Results from './Results'
 
 function Instructions() {
     return (
@@ -123,6 +124,7 @@ export default class Battle extends React.Component {
         this.state = {
             playerOne: null,
             playerTwo: null,
+            battle: false,
         }
         
         this.handleSubmit = this.handleSubmit.bind(this)
@@ -142,7 +144,12 @@ export default class Battle extends React.Component {
     }
 
     render() {
-        const { playerOne, playerTwo } = this.state
+        const { playerOne, playerTwo, battle } = this.state
+
+        if (battle === true ) {
+            return <Results playerOne={playerOne} playerTwo={playerTwo} />
+        }
+
         return (
             <React.Fragment>
                 <Instructions />
@@ -173,6 +180,14 @@ export default class Battle extends React.Component {
                                 />
                         }
                     </div>
+                    {playerOne && playerTwo && (
+                        <button
+                            className='btn dark-btn btn-space'
+                            onClick={() => this.setState({battle:true})}
+                        >
+                            Battle
+                        </button>
+                    )}
                 </div>
                 
             </React.Fragment>
